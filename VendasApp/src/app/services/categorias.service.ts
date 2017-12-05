849afc65272b54029ec7b3635764f66fc1529da8
Jabel Fontoura <jabel_fontoura@hotmail.com>
2017-12-05 10:11:50 -0200

    adicionar categorias service

---
+++ VendasApp/src/app/services/categorias.service.ts
@@ -0,0 +1,46 @@
+import { Categoria } from '../entities/categoria.entity';
+import { Observable } from 'rxjs/Rx';
+import { IService } from './iservice.interface';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from './http-client.service';
+
+@Injectable()
+export class ProdutosService implements IService<Categoria> {
+
+  constructor(private http: HttpClient) { }
+
+  private apiUrl = `${environment.apiUrl}/categorias`;
+
+  public findAll(): Observable<Response> {
+    return this.http.get(this.apiUrl)
+    .map(resp => resp.json())
+    .catch(this.handleError);
+  }
+
+  public findById(id: string): Observable<Response> {
+    return this.http.get(`${this.apiUrl}/${id}`)
+      .map(resp => resp.json())
+      .catch(this.handleError);
+  }
+
+  public add(entity: Categoria): Observable<Response> {
+    return this.http.post(this.apiUrl, entity)
+    .map(resp => resp.json())
+    .catch(this.handleError);
+  }
+
+  public delete(id: string): Observable<Response> {
+    return this.delete(`${this.apiUrl}/${id}`);
+  }
+
+  public update(entity: Categoria): Observable<Response> {
+    return this.http.put(this.apiUrl, entity)
+    .map(resp => resp.json())
+    .catch(this.handleError);
+  }
+
+  private handleError (error: Response | any): Observable<Response> {
+      return Observable.throw(error || 'Server error');
+  }
+}
